93dcd02d131ede6fa614df8cfc47e67f4e4aa3d2
Alessandro Cavalieri <alessandro.cavalieri5@outlook.it>
2020-02-21 15:13:56 +0100

    11

--- leggi.js
+++ leggi.js
@@ -1,2 +1,2 @@
 var f = "(x^345)";
-f = f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\w*/.exec(f.split("^")[0])+","+/\w*/.exec(f.split("^")[1])+")");
+f = f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\b(?:(?!goose)\w)+\b/.exec(f.split("^")[0])+","+/\w*/.exec(f.split("^")[1])+")");
