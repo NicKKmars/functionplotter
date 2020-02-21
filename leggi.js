3fa41f48fe947092d06d002f57029e4fd0fd1f5d
Alessandro Sala <ale9.sala@yahoo.it>
2020-02-21 15:43:49 +0100

    tolto goose

--- leggi.js
+++ leggi.js
@@ -1,5 +1,9 @@
 function converti_funzione(){
     var f = document.getElementById("f_input").value;
-    f = f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\b(?:(.*?)\w)+\b/.exec(f.split("^")[0])+","+/\w*/.exec(f.split("^")[1])+")");
+    f = f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\b(?:(.*?)\w)+\b/.exec(f.split("^")[0])+/\w*/.exec(f.split("^")[1])+")");
     document.getElementById("f_input").value = f;
 }
+
+
+//Stringa con goose
+//f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\b(?:(?!goose)\w)+\b/.exec(f.split("^")[0])+","+/\w*/.exec(f.split("^")[1])+")");
