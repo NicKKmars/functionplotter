c3110d2c1ce23c86707a286690018cbb12d0d84f
Alessandro Sala <ale9.sala@yahoo.it>
2020-02-21 14:15:56 +0100

    OUR FIRST COMMIT!!!! YAYYY!

---
+++ app.js
@@ -0,0 +1,118 @@
+//Classi
+/*
+    Grafico(Colore linea, funzione grafico)
+*/
+function Grafico(color, f) {
+    this.color=color;
+    this.f=f;
+};
+
+
+//Acquisizione elementi html
+var canv=document.getElementById("canv");
+var ctx=canv.getContext("2d");
+var slider = document.getElementById("myRange");
+var lineColor = document.getElementById("lineColor");
+
+
+//definizione costanti
+const width=canv.width;
+const height=canv.height;
+const ratio=width/height;
+
+//definizione variabili
+var currColor="#ff0000";
+var grafici=[];
+console.log(grafici.length);
+//Definisce l'intervallo in cui viene mostrato il grafico [-u; u]
+var initU=6.28;
+var u = initU;      
+
+//Traslazione origine del grafico al centro (L'asse y rimane invertita)
+let tX = width * 0.5,
+tY = height * 0.5;
+ctx.translate(tX, tY);
+ctx.translate(0.5, 0.5);
+
+//EVENTI
+//Zoom con slider
+slider.oninput = function() {
+    u=this.value/10+initU;
+    ctx.clearRect(-tX, -tY, width, height);
+    drawAxes();
+}
+
+lineColor.addEventListener('input', () => {
+    console.log(lineColor.value);
+    currColor=lineColor.value;
+    if(grafici.length>0)
+        grafici[grafici.length-1].color=currColor;
+});
+
+function entryPoint(){
+    console.log("Iniziato");
+    grfici.push(new Grafico(currColor, (x) => {
+        return (eval(document.getElementById("f-tbox").value));
+    }));
+    console.log(grafici.length);
+}
+
+function drawAxes(){
+    //Intervallo di rappresentazione
+    ctx.font = "15px Arial";
+    ctx.fillText(Math.round(-u*100)/100, -width/2+10, -10);
+    ctx.fillText(Math.round(u*100)/100, width/2-40, -10);
+
+    ctx.beginPath();
+    ctx.strokeStyle='#888';
+    ctx.moveTo(-width/2, 0);
+    ctx.lineTo(width/2, 0);
+    
+    ctx.moveTo(0, -height/2);
+    ctx.lineTo(0, height/2);
+    
+    //Griglia
+    var num=6;  //Numero linee
+    for(let i=0; i<num; i++){
+        let x_comp=width*i/num-width/2;
+        //SPERIMENTALE
+        //ctx.fillText(u/(num/2), x_comp, -10);
+        //FINE
+        ctx.moveTo(x_comp, -height/2);
+        ctx.lineTo(x_comp, height/2);
+    }
+    for(let i=0; i<num; i++){
+        let y_comp = height*i/num-height/2;
+        //SPERIMENTALE
+        //ctx.fillText(Math.round(-u*100)/100, -width/2+10, -10);
+        //FINE
+        ctx.moveTo(-width/2, y_comp);
+        ctx.lineTo(width/2, y_comp);
+    }
+    ctx.stroke();
+    ctx.closePath();
+
+
+}
+
+
+function drawGraph(){
+    for(let j=1; j<grafici.length; j++){
+        console.log(grafici[j]);
+        ctx.beginPath();
+        ctx.strokeStyle = grafici[j].color;
+        //ctx.moveTo(-10000, 0);
+        for(let i=-width/2; i<width/2; i++){
+            let x=i*u/(width/2);
+            //console.log(x, f(x));
+            //console.log(i, -(f(x))*height/2);
+            ctx.lineTo(i, -(grafici[j].f(x))*(width/(2*u)));
+            ctx.stroke();
+        }
+        ctx.closePath();
+    }   
+}
+
+setInterval(drawGraph, 1000/60);
+
+drawAxes();
