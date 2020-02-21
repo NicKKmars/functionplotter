a737ea688281bcf6a29445e51b191069112382c1
Alessandro Sala <ale9.sala@yahoo.it>
2020-02-21 15:48:00 +0100

    pattern in vars

--- leggi.js
+++ leggi.js
@@ -1,6 +1,9 @@
 function converti_funzione(){
     var f = document.getElementById("f_input").value;
-    f = f.replace(/\w*\^\w*/.exec(f),"Math.pow("+/\b(?:(?!goose)\w)+\b/.exec(f.split("^")[0])+","+/\w*/.exec(f.split("^")[1])+")");
+    let p1=/\w*\^\w*/;
+    let p2=/\b(?:(?!goose)\w)+\b/;
+    let p3=/\w*/;
+    f = f.replace(p1.exec(f),"Math.pow("+p2.exec(f.split("^")[0])+","+p3.exec(f.split("^")[1])+")");
     document.getElementById("f_input").value = f;
 }
 
