8260e816256cfddf12c2e46f5b455a068fe08d98
Alessandro Sala <ale9.sala@yahoo.it>
2020-03-08 17:18:53 +0100

    commenti

--- leggi.js
+++ leggi.js
@@ -1,7 +1,8 @@
+//Pagina che consente la sperimentazione delle regex - da integrare con l'applicazione principale (app.js)
 function converti_funzione(){
     var f = document.getElementById("f_input").value;
     let p1=/\w*\^\w*/;
-    let p2=/\b(?:(?!goose)\w)+\b/;
+    let p2=/\b(?:(?! )\w)+\b/;
     let p3=/\w*/;
     f = f.replace(p1.exec(f),"Math.pow("+p2.exec(f.split("^")[0])+","+p3.exec(f.split("^")[1])+")");
     document.getElementById("f_input").value = f;
